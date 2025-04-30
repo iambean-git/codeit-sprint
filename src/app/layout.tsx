4a962858195286e39d287968958c094d6e8012d0
iambean-git <peb1021@naver.com>
2025-04-30 09:23:58 +0900

    단어 변경 애니메이션션

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 // app/page.tsx
 
-import Link from "next/link";
-import PageTransition from "@/components/PageTransition";
+import WordChangeExample from "./components/WordChangeExample";
 
 export default function Page() {
   return (
-    <PageTransition>
-      <div>
-        <Link className="text-blue-500 underline" href="/about">
-          About으로
-        </Link>
-        {/* 코드 생략*/}
-      </div>
-    </PageTransition>
+    <>
+      <WordChangeExample />
+    </>
   );
 }
