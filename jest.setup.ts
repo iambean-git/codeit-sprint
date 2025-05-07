41cd4d763238da0e957178dc7b5201552296b77e
iambean-git <peb1021@naver.com>
2025-05-07 11:28:52 +0900

    install Jest-Extended Matcher

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,3 +5,5 @@
 // 3. 일관성 유지: 모든 테스트에 동일한 확장 기능이 적용되므로 테스트 코드가 일관성을 유지합니다.
 // 4. 설정 집중화: 테스트 환경 설정을 한 곳에서 관리할 수 있어 나중에 변경이 필요할 때 편리합니다.
 import "@testing-library/jest-dom";
+import * as matchers from "jest-extended";
+expect.extend(matchers);
